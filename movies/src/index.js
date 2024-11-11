459548d9cfd3b73cca3ca9c6419981411fd53ed8
dominikakmiecik <20093309@mail.wit.ie>
2024-11-11 10:41:09 +0000

    Movie Details Page rendering.

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import HomePage from "./pages/homePage";
+import MovieDetailsPage from './pages/movieDetailsPage'
+
+const images = [
+    "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+    "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
+    "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
+    "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
+] 
 
 const sample = {
     adult: false,
@@ -11,6 +19,8 @@ const sample = {
         poster_path: "/iTQHKziZy9pAAY4hHEDCGPaOvFC.jpg",
         backdrop_path: "/d8duYyyC9J5T825Hg7grmaabfxQ.jpg",
     },
+
+
     budget: 200000000,
     genres: [
         {
@@ -87,7 +97,7 @@ const movies = [sample, sample, sample, sample, sample, sample, sample];
 
 const App = () => {
     return (
-        <HomePage movies={movies} />
+        <MovieDetailsPage movie={sample} images={images} />
     );
 };
 
